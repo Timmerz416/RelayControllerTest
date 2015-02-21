0f8b78e4b5a5771096f8d8c9ac86ecc600ab1f6c
Tim Lampman <timothy.lampman@gmail.com>
2015-02-21 17:16:33 -0500

    Added GBee library and linkages

--- Relay Controller Test/Program.cs
+++ Relay Controller Test/Program.cs
@@ -4,6 +4,9 @@ using Microsoft.SPOT;
 using Microsoft.SPOT.Hardware;
 using SecretLabs.NETMF.Hardware;
 using SecretLabs.NETMF.Hardware.Netduino;
+using NETMF.OpenSource.XBee;
+using NETMF.OpenSource.XBee.Api;
+using NETMF.OpenSource.XBee.Api.Zigbee;
 
 namespace RelayControllerTest {
 	public class Program {
